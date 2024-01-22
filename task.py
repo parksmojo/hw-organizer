2c798c7b90f25619511a21403e9a84fa8a50c863
Parker <parkersam@gmail.com>
2024-01-22 16:29:20 -0700

    Implement task.py constructor and __str__() - other formatting issues

--- task.py
+++ task.py
@@ -1,3 +1,19 @@
 class Task:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, course: str, name: str, due, prep: int, priority, done: bool) -> None:
+        self.course = course
+        self.name = name
+        self.due = due
+        self.prep = prep
+        self.priority = priority
+        self.done = done
+
+    def __str__(self):
+        if self.done:
+            finished = 'Completed'
+        else:
+            finished = 'Not Completed'
+
+        start = self.due - self.prep
+
+        return (f"{self.course}, \"{self.name}\", Due by:{self.due}, "
+                f"Start by:{start}, {self.priority} priority, {finished}")
