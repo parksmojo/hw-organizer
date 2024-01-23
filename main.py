624fb74d00f35105bf2d25adfac817a1b7b6870c
Parker <parkersam@gmail.com>
2024-01-23 14:30:12 -0700

    Basic Organizer method implementation - Constructor - addTask() - __str__()

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from organizer import Organizer
 from task import Task
 
 if __name__ == "__main__":
-    tasks = [Task('Rel','Weekly Practice',2),
-             Task('CS240','Phase 1',14,7),
-             Task('CS202','Lab 3',3)]
-    for item in tasks:
-        print(item)
-    pass
+    hwo = Organizer()
+
+    hwo.addTask('Rel','Weekly Practice',2)
+    hwo.addTask('CS240','Phase 1',14,7)
+    hwo.addTask('CS202','Lab 3',3)
+
+    print(hwo)
--- organizer.py
+++ organizer.py
@@ -1,3 +1,15 @@
+from task import Task
+
 class Organizer:
     def __init__(self) -> None:
-        pass
+        self.tasks: set = set()
+    
+    def addTask(self, course, name, due, prep=0, priority=0):
+        self.tasks.add(Task(course, name, due, prep, priority))
+
+    def __str__(self):
+        output: str = ''
+        for item in self.tasks:
+            output += item.__str__()
+            output += '\n'
+        return output
