0bb46f5d110325e07588a7f1fcc41a13ab753c8b
Parker <parkersam@gmail.com>
2024-01-23 21:06:42 -0700

    Reworked __str__()

--- task.py
+++ task.py
@@ -4,19 +4,35 @@ class Task:
         self.name = name
         self.due = due
         self.prep = prep
-        self.priority = priority
         self.done = done
+
+        self.priorities_list: list[str] = [None,'Low','Medium','High']
+        if priority < 0:
+            self.priority = 0
+        elif priority > 2:
+            self.priority = 2
+        else:
+            self.priority = priority
 
     def complete(self):
         self.done = True
 
     def __str__(self):
+        output = ''
+        output += f'{self.course}'
+        output += f', \"{self.name}\"\n'
+        for i in range(len(f'{self.course}, ')):
+            output += ' '
+        output += f'-Due in {self.due} days'
+
+        if self.prep != 0:
+            output += f', Start in {self.due - self.prep} days'
+        
+        if self.priority != 0:
+            output += f', {self.priorities_list[self.priority]} priority'
+        
         if self.done:
-            done: str = ', Completed'
-        else:
-            done: str = ''
-
-        start = self.due - self.prep
-
-        return (f"{self.course}, \"{self.name}\", Due in {self.due} days, "
-                f"Start in {start} days, {self.priority} priority{done}")
+            output += ', Completed'
+        
+        # output += '\n'
+        return output
