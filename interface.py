d3c731fed5e84865d24bd784486a58244a2727e9
Parker <parkersam@gmail.com>
2024-01-25 14:46:54 -0700

    Integrated print organizer function into interface

--- interface.py
+++ interface.py
@@ -3,13 +3,17 @@ from organizer import Organizer
 class Interface:
     def __init__(self) -> None:
         self.running = True
+        self.hwo = Organizer()
         self.process()
 
     def process(self):
         while(self.running):
             usrinput = input("Command: ")
 
-            if usrinput == "quit":
-                self.running = False
-            else:
-                print(usrinput)
+            match usrinput:
+                case "quit" | "Quit" | 'q':
+                    self.running = False
+                case 'print' | 'Print' | 'p':
+                    print(self.hwo)
+                case _ :
+                    print(f"Command not recognized: {usrinput}")
